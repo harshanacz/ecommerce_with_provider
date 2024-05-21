ed35d62b1ca911b24242e7cd34ceb0ed4f652e73
Kavindu Harshana <45476434+harshanacz@users.noreply.github.com>
2024-05-21 19:13:19 +0530

    lets setup provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerceapp/providers/products_provider.dart';
 import 'package:ecommerceapp/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
@@ -12,9 +14,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: appRoutes,
+    return ChangeNotifierProvider(
+      create: (context) => ProductProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: appRoutes,
+      ),
     );
   }
 }
